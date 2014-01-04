a82790a42614e5a0df22fa37b856d3ab91e9f21d
Bryan St. Amour <bryan.stamour@gmail.com>
2014-01-04 17:00:55 -0500

    comment. Because comment.

--- bin/getvolume.sh
+++ bin/getvolume.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#
+# Not guaranteed to work on all systems, but it works for me.
+#
+
 case $1 in
   "level")
     amixer sget Master,0 |grep -E \[.*\%\] | awk '{print $4}' | sed -r 's/(\[|\])//g'
@@ -7,6 +11,7 @@ case $1 in
   "mute")
     amixer sget Master,0 |grep -E \[.*\%\] | awk '{print $6}' | sed -r 's/(\[|\])//g'
   ;;
+  *)
+    echo "Usage: $0 [level|mute]"
+  ;;
 esac
-
-
