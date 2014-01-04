5b231fe713adc7cd8efe1a7f48f2d0a1bb551046
Bryan St. Amour <bryan.stamour@gmail.com>
2014-01-04 17:07:16 -0500

    removed code duplication.

--- bin/getvolume.sh
+++ bin/getvolume.sh
@@ -4,14 +4,19 @@
 # Not guaranteed to work on all systems, but it works for me.
 #
 
+field=''
+
 case $1 in
   "level")
-    amixer sget Master,0 |grep -E \[.*\%\] | awk '{print $4}' | sed -r 's/(\[|\])//g'
+    field='$4'
   ;;
   "mute")
-    amixer sget Master,0 |grep -E \[.*\%\] | awk '{print $6}' | sed -r 's/(\[|\])//g'
+    field='$6'
   ;;
   *)
     echo "Usage: $0 [level|mute]"
+    exit 1
   ;;
 esac
+
+amixer sget Master,0 |grep -E \[.*\%\] | awk "{print $field}" | sed -r 's/(\[|\])//g'
