ac59725f82601c4131f287cf48aa122e50b19f5f
Bryan St. Amour <bryan.stamour@gmail.com>
2014-02-08 12:30:01 -0500

    lots of changes to code layout. New key strokes for workspace management.

--- xmonad.hs
+++ xmonad.hs
@@ -1,22 +1,25 @@
 import XMonad
+
 import XMonad.Actions.SpawnOn
+import XMonad.Actions.CycleWS
+
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+
 import XMonad.Layout.Grid
 import XMonad.Layout.Spacing
 import XMonad.Layout.Renamed
 import XMonad.Layout.PerWorkspace (onWorkspace, onWorkspaces)
+
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
 
 import Padding
 import System.IO
 
-
 main = do
   xmproc <- spawnPipe "/home/bryan/.cabal/bin/xmobar /home/bryan/.xmonad/xmobarrc"
   xmonad $ myConfig xmproc `additionalKeys` myAdditionalKeys
-
 
 myConfig xmproc =
   defaultConfig
@@ -31,68 +34,61 @@ myConfig xmproc =
   , workspaces         = myWorkspaces
   }
 
-
-myModMask = mod1Mask
-
+myModMask = mod4Mask
 
 myTerminal = "urxvt"
 
-
-myManageHook = manageDocks        <+>
-               manageSpawn        <+>
-               composeAll myRules <+>
-               manageHook defaultConfig
+myManageHook = manageDocks
+               <+> manageSpawn
+               <+> composeAll myRules
+               <+> manageHook defaultConfig
   where
     myRules = [ title     =? "mutt"    --> doShift "1:mail"
               , className =? "Firefox" --> doShift "2:web"
               , className =? "Emacs"   --> doShift "3:code"
               ]
 
-
 myWorkspaces = ["1:mail", "2:web", "3:code", "4:terms", "5", "6" ,"7", "8", "9","10"]
 
-
-myLayoutHook = avoidStruts                 $
-               onWorkspace "2:web" web     $
-               onWorkspace "4:terms" terms $
-               standards
+myLayoutHook = avoidStruts
+               $ onWorkspace "2:web" fullscreen
+               $ onWorkspace "4:terms" terms
+               $ standards
   where
-    -- Per-workspace layout schemes.
     standards  = tiled ||| Mirror tiled ||| Full
-    web        = paddedFull ||| Full ||| tiled ||| Mirror tiled
     fullscreen = Full ||| tiled ||| Mirror tiled
     terms      = Grid ||| tiled ||| Full
-
-    -- Layouts.
-    paddedFull  = renamed [Replace "Padded"]  $ padding 0 200  Full
-    tiled       = renamed [Replace "Default"] $ smartSpacing 5 $ Tall nmaster delta ratio
-      where
-        nmaster = 1
-        ratio   = 1/2
-        delta   = 3/100
-
+    tiled      = renamed [Replace "Default"] $ smartSpacing 20 $ Tall nmaster delta ratio
+    nmaster    = 1
+    ratio      = 1/2
+    delta      = 3/100
 
 myLogHook xmproc = dynamicLogWithPP xmobarPP
                      { ppOutput = hPutStrLn xmproc
                      , ppTitle  = xmobarColor "green" "" . shorten 50
                      }
 
-
 -- The little launch key next to the mic key at the top.
 myLaunchKey = 0x1008FF41
-
 
 -- Volume control keys.
 myRaiseVolumeKey = 0x1008ff13
 myLowerVolumeKey = 0x1008ff11
 myMuteVolumeKey  = 0x1008ff12
 
-
 myAdditionalKeys =
-  [ ((myModMask .|. shiftMask, xK_p), spawn "sudo /usr/sbin/pm-suspend")
-  , ((0, myLaunchKey),                spawn "sudo /sbin/shutdown -h now")
-  , ((0, myMuteVolumeKey),            spawn "amixer set Master toggle")
-  , ((0, myRaiseVolumeKey),           spawn "amixer set Master,0 2%+")
-  , ((0, myLowerVolumeKey),           spawn "amixer set Master,0 2%-")
-  , ((myModMask, xK_m),               spawn (myTerminal ++ " -e mutt"))
+  [ ((modShift, xK_p),      spawn "sudo /usr/sbin/pm-suspend")  -- Suspend
+  , ((0, myLaunchKey),      spawn "sudo /sbin/shutdown -h now") -- Shutdown
+  , ((0, myMuteVolumeKey),  spawn "amixer set Master toggle")   -- Toggle sound mute
+  , ((0, myRaiseVolumeKey), spawn "amixer set Master,0 2%+")    -- Raise volume
+  , ((0, myLowerVolumeKey), spawn "amixer set Master,0 2%-")    -- Lower volume
+  , ((mod, xK_m),           spawn (myTerminal ++ " -e mutt"))   -- Launch mail
+  , ((mod, xK_Tab),         toggleWS)                           -- toggle last workspace (super-tab)
+  , ((mod, xK_Right),       nextWS)                             -- go to next workspace
+  , ((mod, xK_Left),        prevWS)                             -- go to prev workspace
+  , ((modShift, xK_Right),  shiftToNext)                        -- move client to next workspace
+  , ((modShift, xK_Left),   shiftToPrev)                        -- move client to prev workspace
   ]
+  where
+    mod      = myModMask
+    modShift = myModMask .|. shiftMask
