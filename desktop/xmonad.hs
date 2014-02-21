f87a1fb6aa8917adf3136f9ff8d9e8111b31770c
Bryan St. Amour <bryan@bryanstamour.com>
2014-02-20 21:37:14 -0500

    cleaned up for desktop. Don't need as many custom layouts with such a huge monitor.

--- desktop/xmonad.hs
+++ desktop/xmonad.hs
@@ -8,8 +8,8 @@ import XMonad.Hooks.ManageDocks
 
 import XMonad.Layout.Grid
 import XMonad.Layout.Spacing
+import XMonad.Layout.SimpleFloat
 import XMonad.Layout.Renamed
-import XMonad.Layout.PerWorkspace (onWorkspace, onWorkspaces)
 
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
@@ -30,7 +30,6 @@ myConfig xmproc =
   , normalBorderColor  = "#444444"
   , focusedBorderColor = "#FF0000"
   , borderWidth        = 1
-  , workspaces         = myWorkspaces
   }
 
 myModMask = mod4Mask
@@ -39,29 +38,11 @@ myTerminal = "urxvt"
 
 myManageHook = manageDocks
                <+> manageSpawn
-               <+> composeAll myRules
                <+> manageHook defaultConfig
-  where
-    myRules = [ className =? "Firefox"     --> doShift "2:web"
-              , className =? "Chromium"    --> doShift "2:web"
-              , className =? "Thunderbird" --> doShift "2:web"
-              , className =? "Emacs"       --> doShift "1:emacs"
-              ]
 
-myWorkspaces = zipWith (++) numbers (labels ++ blanks)
-  where
-    numbers = map show [1.. 9]
-    blanks  = "" : blanks
-    labels  = [":emacs", ":web", ":terms"]
-
-myLayoutHook = avoidStruts
-               $ onWorkspace "2:web" fullscreen
-               $ onWorkspace "4:terms" terms
-               $ standards
+myLayoutHook = avoidStruts $ standards
   where
     standards  = tiled ||| Mirror tiled ||| Full
-    fullscreen = Full ||| tiled ||| Mirror tiled
-    terms      = Grid ||| tiled ||| Full
     tiled      = renamed [Replace "Default"] $ smartSpacing 20 $ Tall nmaster delta ratio
     nmaster    = 1
     ratio      = 1/2
