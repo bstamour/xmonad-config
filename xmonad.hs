de18f8a8bad5330704e2db44c6bbe540e5537d52
Bryan St. Amour <bryan@bryanstamour.com>
2014-12-11 19:24:43 -0500

    moved laptop stuff into main dir

--- xmonad.hs
+++ xmonad.hs
@@ -1,24 +1,30 @@
-import XMonad
+{-# LANGUAGE TemplateHaskell #-}
 
+
+import XMonad
 import XMonad.Actions.SpawnOn
 import XMonad.Actions.CycleWS
-
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-
 import XMonad.Layout.Grid
 import XMonad.Layout.Spacing
 import XMonad.Layout.Renamed
 import XMonad.Layout.PerWorkspace (onWorkspace, onWorkspaces)
-
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
 
 import System.IO
+import Language.Haskell.TH
+import Network.HostName
+
+
+[d| myHostName = $(stringE =<< runIO getHostName ) |]
+
 
 main = do
   xmproc <- spawnPipe "/home/bryan/.cabal/bin/xmobar /home/bryan/.xmonad/xmobarrc"
   xmonad $ myConfig xmproc `additionalKeys` myAdditionalKeys
+
 
 myConfig xmproc =
   defaultConfig
@@ -30,12 +36,14 @@ myConfig xmproc =
   , normalBorderColor  = "#444444"
   , focusedBorderColor = "#FF0000"
   , borderWidth        = 1
-  , workspaces         = myWorkspaces
   }
+
 
 myModMask = mod4Mask
 
+
 myTerminal = "urxvt"
+
 
 myManageHook = manageDocks
                <+> manageSpawn
@@ -48,45 +56,51 @@ myManageHook = manageDocks
               , resource  =? "mutt"     --> doShift "3:comm"
               ]
 
+
 myWorkspaces = zipWith (++) numbers (labels ++ blanks)
   where
     numbers = map show [1.. 9]
     blanks  = "" : blanks
     labels  = [":emacs", ":web", ":comm", ":terms"]
 
-myLayoutHook = avoidStruts
-               $ onWorkspace "2:web" fullscreen
-               $ onWorkspace "4:terms" terms
-               $ standards
+
+myLayoutHook = avoidStruts $ standards
   where
     standards  = tiled ||| Mirror tiled ||| Full
     fullscreen = Full ||| tiled ||| Mirror tiled
     terms      = Grid ||| tiled ||| Full
-    tiled      = renamed [Replace "Default"] $ Tall nmaster delta ratio
+    tiled      = renamed [Replace "Default"]
+                 $ smartSpacing spacing
+                 $ Tall nmaster delta ratio
     nmaster    = 1
     ratio      = 1/2
     delta      = 3/100
+    spacing = case myHostName of
+      "cantor" -> 0
+      _        -> 20
+
 
 myLogHook xmproc = dynamicLogWithPP xmobarPP
                      { ppOutput = hPutStrLn xmproc
                      , ppTitle  = xmobarColor "green" "" . shorten 50
                      }
 
+
 -- The little launch key next to the mic key at the top.
 myLaunchKey = 0x1008FF41
+
 
 -- Volume control keys.
 myRaiseVolumeKey = 0x1008ff13
 myLowerVolumeKey = 0x1008ff11
 myMuteVolumeKey  = 0x1008ff12
 
+
 myAdditionalKeys =
   [ ((modShift, xK_p),      spawn "sudo /usr/sbin/pm-suspend")
-  , ((0, myLaunchKey),      spawn "sudo /sbin/shutdown -h now")
   , ((0, myMuteVolumeKey),  spawn "amixer set Master toggle")
   , ((0, myRaiseVolumeKey), spawn "amixer set Master,0 2%+")
   , ((0, myLowerVolumeKey), spawn "amixer set Master,0 2%-")
-  , ((mod, xK_m),           spawn (myTerminal ++ " -name mutt -e mutt"))
   , ((mod, xK_Tab),         toggleWS)
   , ((mod, xK_Right),       nextWS)
   , ((mod, xK_Left),        prevWS)
